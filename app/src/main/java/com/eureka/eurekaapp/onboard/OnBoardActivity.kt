a517dabbec78bce68c8445ec5fcb5d7982664bcc
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2021-03-18 15:17:51 +0800

    add onboard screen

---
+++ app/src/main/java/com/eureka/eurekaapp/onboard/OnBoardActivity.kt
@@ -0,0 +1,29 @@
+package com.eureka.eurekaapp.onboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eureka.eurekaapp.R
+import com.eureka.eurekaapp.base.BaseActivity
+import com.eureka.eurekaapp.databinding.ActivityOnBoardBinding
+import com.eureka.eurekaapp.login.LoginActivity
+import com.eureka.eurekaapp.signup.SignUpActivity
+
+class OnBoardActivity : BaseActivity<ActivityOnBoardBinding>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setLayout = R.layout.activity_on_board
+        super.onCreate(savedInstanceState)
+
+        onClickListener()
+    }
+
+    private fun onClickListener() {
+        binding.btnLogin.setOnClickListener {
+            intent<LoginActivity>(this)
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            intent<SignUpActivity>(this)
+        }
+
+    }
+}
